8a0ed754b0f2799d504c8eba69f2cbf17dc78067
Robert Harrison <harrisonrw@gmail.com>
2019-05-17 15:49:55 -0700

    Initial Project Files

---
+++ SwiftRun/SwiftRunTests/SwiftRunTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftRunTests.swift
+//  SwiftRunTests
+//
+//  Created by Robert Harrison on 5/17/19.
+//  Copyright © Robert Harrison. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftRun
+
+class SwiftRunTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
