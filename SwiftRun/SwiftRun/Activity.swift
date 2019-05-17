ee8e25e216088f3aa2f22f68d095be1c290c32ba
Robert Harrison <harrisonrw@gmail.com>
2019-05-17 16:11:55 -0700

    Add Activity

---
+++ SwiftRun/SwiftRun/Activity.swift
@@ -0,0 +1,35 @@
+//
+//  Activity.swift
+//  SwiftRun
+//
+//  Created by Robert Harrison on 5/17/19.
+//  Copyright © 2019 Robert Harrison. All rights reserved.
+//
+
+import Foundation
+
+enum ActivityState: String {
+    case active
+    case paused
+    case complete
+}
+
+class Activity {
+
+    var date: Date
+    var duration: Double         // In Seconds
+    var distance: Double         // In Meters
+    var activityState: ActivityState
+
+    var endDate: Date {
+        return date.addingTimeInterval(duration)
+    }
+
+    init(date: Date, duration: Double, distance: Double, activityState: ActivityState) {
+        self.date = date
+        self.duration = duration
+        self.distance = distance
+        self.activityState = activityState
+    }
+
+}
