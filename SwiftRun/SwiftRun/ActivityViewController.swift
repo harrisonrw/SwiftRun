d082b4f68d2b602e1c319b1630dd2e0af0ec934e
Robert Harrison <harrisonrw@gmail.com>
2019-05-17 17:02:24 -0700

    Activity View Controller with Labels and Buttons

---
+++ SwiftRun/SwiftRun/ActivityViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ActivityViewController.swift
+//  SwiftRun
+//
+//  Created by Robert Harrison on 5/17/19.
+//  Copyright © 2019 Robert Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    @IBOutlet private var durationLabel: UILabel!
+    @IBOutlet private var distanceLabel: UILabel!
+    @IBOutlet private var startButton: UIButton!
+    @IBOutlet private var stopButton: UIButton!
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Actions
+
+    @IBAction func start() {
+
+    }
+
+    @IBAction func stop() {
+
+    }
+}
