332afa3d020ee0f6089a35295ec78aa76ab7e3a1
Robert Harrison <harrisonrw@gmail.com>
2019-05-20 11:58:30 -0700

    Wire up the Activity ViewController to the ViewModel and LogicController

--- SwiftRun/SwiftRun/ActivityViewController.swift
+++ SwiftRun/SwiftRun/ActivityViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import os
 
-class ActivityViewController: UIViewController {
+class ActivityViewController: UIViewController, ActivityLogicControllerDelegate {
 
     @IBOutlet private var durationLabel: UILabel!
     @IBOutlet private var distanceLabel: UILabel!
@@ -17,6 +18,8 @@ class ActivityViewController: UIViewController {
 
     private var environment: Environment!
     private var activityLogicController: ActivityLogicController!
+
+    private var viewModel: ActivityViewModel?
 
     // MARK: - Configuration
 
@@ -42,15 +45,68 @@ class ActivityViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "SwiftRun"
+
+        activityLogicController.delegate = self
+
+        viewModel = ActivityViewModel { [weak self] state in
+            guard let strongSelf = self else { return }
+            strongSelf.refreshView(with: state)
+        }
+        viewModel!.reset()
+    }
+
+    // MARK: - ViewModel Callback
+
+    private func refreshView(with state: ActivityViewModel.State) {
+
+        assert(Thread.isMainThread)
+
+        durationLabel.text = state.duration
+        distanceLabel.text = state.distance
+
+        startButton.isHidden = !state.actions.startEnabled
+        stopButton.isHidden = !state.actions.stopEnabled
     }
 
     // MARK: - Actions
 
     @IBAction func start() {
 
+        activityLogicController.startActivity { [weak self] activity, error in
+
+            guard error == nil else {
+                self?.handleError(error!)
+                return
+            }
+
+            self?.viewModel?.update(with: activity)
+        }
+
     }
 
     @IBAction func stop() {
 
+        activityLogicController.stopActivity { [weak self] activity in
+            self?.viewModel?.update(with: activity)
+        }
+
+    }
+
+    private func handleError(_ error: AppErrorType) {
+
+        if case AppError.Location.locationAuthorizationNotDetermined = error {
+            assert(Thread.isMainThread)
+            activityLogicController.requestLocationAuthorization()
+        } else {
+            // TODO: Present error message to the user
+            os_log("Error: %@", log: .default, type: .error, error.debugDescription)
+        }
+    }
+
+    // MARK: - ActivityLogicControllerDelegate
+
+    func activityLogicController(_ activityLogicController: ActivityLogicController,
+                                 didUpdateActivity activity: Activity) {
+        viewModel?.update(with: activity)
     }
 }
